26e3c93405b588c9e800110d2d76fb82b2e1e0b1
Giorgi1123 <77487548+Giorgi1123@users.noreply.github.com>
2021-07-27 10:53:49 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FOR_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetRequest("http://localhost:3030/");
+            
+
+            Console.ReadKey();
+
+        }
+        async static Task GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
